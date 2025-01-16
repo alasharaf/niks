b20d10a35a98f6d92f8923ee1bfdf64b70a1110d
Ala Sharaf <3la2sharaf@gmail.com>
2025-01-16 18:56:32 +0400

    remove footer

--- web/app/(commonLayout)/apps/page.tsx
+++ web/app/(commonLayout)/apps/page.tsx
@@ -15,7 +15,6 @@ const AppList = () => {
   return (
     <div className='relative flex flex-col overflow-y-auto bg-background-body shrink-0 h-0 grow'>
       <Apps />
-      {systemFeatures.license.status === LicenseStatus.NONE && <footer className='px-12 py-6 grow-0 shrink-0'>
     
     </div >
   )
